a1ed53d37dc6925ecf61e02105a877873d0a23fb
Tim Weißenfels <tim.weissenfels@outlook.com>
2018-06-25 18:09:05 +0200

    create session methods

--- src/core/SessionManager.ts
+++ src/core/SessionManager.ts
@@ -6,7 +6,8 @@ export type ASMCommand = "openSession";
 var activeSessions:ActiveSessionList = new ActiveSessionList();
 
 var config = {
-	newWindow: true
+	newWindow: true,
+	parentBookmarkId: ""
 };
 
 export function openSession(sessionId:string, newWindow = config.newWindow):Promise<void> {
@@ -36,4 +37,44 @@ export function openSession(sessionId:string, newWindow = config.newWindow):Prom
 
 		return session.openAll();
 	});
+}
+
+export function createSessionFromTabs(title:string, tabs:browser.tabs.Tab[]):Promise<string> {
+	if(tabs.length === 0) {
+		return Promise.reject();
+	}
+
+	return browser.bookmarks.create({
+		parentId: config.parentBookmarkId,
+		title: title
+	}).then(folder => {
+		let sessionFolderId:string = folder.id;
+
+		return Promise.all(
+			tabs.map(
+				tab => browser.bookmarks.create({
+					parentId: sessionFolderId,
+					title: title,
+					url: tab.url
+				})
+			)
+		).then(_ => {
+			return sessionFolderId;
+		});
+	});
+}
+
+export function createSessionFromWindow(title:string, windowId?:number):Promise<string> {
+	if(windowId === undefined) {
+		windowId = browser.windows.WINDOW_ID_CURRENT;
+	}
+
+	// tab search query
+	// https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/tabs/query
+	let query = {
+		windowId: windowId
+	};
+
+	//TODO: filter
+	return browser.tabs.query(query).then(tabs => createSessionFromTabs(title, tabs));
 }
