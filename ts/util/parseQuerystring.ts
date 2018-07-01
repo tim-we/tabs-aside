0b7d0027705542969fdaf5fe3595e6e329dd0c63
Tim Weißenfels <tim.weissenfels@outlook.com>
2018-07-01 14:23:42 +0200

    added parseQueryString utility

---
+++ ts/util/parseQuerystring.ts
@@ -0,0 +1,13 @@
+export default function parseQueryString() {
+	let objURL = {};
+
+	window.location.search.replace(
+		new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+		//@ts-ignore
+		($0, $1, $2, $3) => { objURL[ $1 ] = $3; }
+	);
+
+	return objURL;
+};
+
+// let params = parseQueryString(); params["foo"] = "bar" ...
