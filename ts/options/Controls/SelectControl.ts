10f997abd37d9f398741b27a563237ce7c27bab2
Tim Weißenfels <tim.weissenfels@outlook.com>
2018-07-06 18:08:00 +0200

    refactored select control

---
+++ ts/options/Controls/SelectControl.ts
@@ -0,0 +1,38 @@
+import { SelectOption } from "../OptionTypeDefinition";
+import * as OptionsManager from "../OptionsManager";
+
+export function create(
+    row:HTMLDivElement,
+    i:number,
+    option:SelectOption, value:string,
+    i18nMessageName:string
+):void {
+    let select:HTMLSelectElement = document.createElement("select");
+	select.id = "select" + i;
+	
+	let selectOptions = option.options;
+
+	selectOptions.forEach(o => {
+		let selectOption:HTMLOptionElement = document.createElement("option");
+		selectOption.value = o;
+		selectOption.innerText = browser.i18n.getMessage(i18nMessageName + "__" + o) || "@ERROR";
+		
+		if(value === o) {
+			selectOption.selected = true;
+		}
+
+		select.appendChild(selectOption);
+	});
+
+	let label:HTMLLabelElement = document.createElement("label");
+	label.setAttribute("for", select.id);
+	label.innerText = browser.i18n.getMessage(i18nMessageName) || "empty";
+
+	select.addEventListener("change", () => {
+		let x = select.options[select.selectedIndex].value;
+		OptionsManager.setValue(option.id, x);
+	});
+
+	row.appendChild(label);
+	row.appendChild(select);
+}
--- ts/options/OptionsPage.ts
+++ ts/options/OptionsPage.ts
@@ -1,8 +1,10 @@
-import Options from "./Options";
 import * as OptionsManager from "./OptionsManager";
-import { Option, SelectOption } from "./OptionTypeDefinition";
+import { Option } from "./OptionTypeDefinition";
+import Options from "./Options";
+
 import * as BooleanControl from "./Controls/BooleanControl";
 import * as BookmarkControl from "./Controls/BookmarkControl";
+import * as SelectControl from "./Controls/SelectControl";
 
 document.addEventListener("DOMContentLoaded", () => {
 	let section = document.getElementById("main-section");
@@ -32,11 +34,10 @@ document.addEventListener("DOMContentLoaded", () => {
 		} else if(option.type === "select") {
 			let value:string = await OptionsManager.getValue<string>(option.id);
 
-			selectOptionView(
-				row,
-				option.id, option, value,
-				i18nMessageName,
-				i
+			SelectControl.create(
+				row, i,
+				option, value,
+				i18nMessageName
 			);
 		} else if(option.type === "bookmark") {
 			let value:string = await OptionsManager.getValue<string>(option.id);
@@ -69,39 +70,3 @@ document.addEventListener("DOMContentLoaded", () => {
 		i++;
 	});
 });
-
-function selectOptionView(
-	row:HTMLDivElement,
-	optionKey:string, option:SelectOption, value:string,
-	i18nMessageName:string,
-	i:number
-):void {
-	let select:HTMLSelectElement = document.createElement("select");
-	select.id = "select" + i;
-	
-	let selectOptions = option.options;
-
-	selectOptions.forEach(o => {
-		let selectOption:HTMLOptionElement = document.createElement("option");
-		selectOption.value = o;
-		selectOption.innerText = browser.i18n.getMessage(i18nMessageName + "__" + o) || "@ERROR";
-		
-		if(value === o) {
-			selectOption.selected = true;
-		}
-
-		select.appendChild(selectOption);
-	});
-
-	let label:HTMLLabelElement = document.createElement("label");
-	label.setAttribute("for", select.id);
-	label.innerText = browser.i18n.getMessage(i18nMessageName) || "empty";
-
-	select.addEventListener("change", () => {
-		let x = select.options[select.selectedIndex].value;
-		OptionsManager.setValue(optionKey, x);
-	});
-
-	row.appendChild(label);
-	row.appendChild(select);
-}
