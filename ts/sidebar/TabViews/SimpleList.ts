7db3146ddf1a58d79cb96560ef1e486099df957b
Tim Weißenfels <stu126998@mail.uni-kiel.de>
2018-07-09 20:24:28 +0200

    fix for #35

--- ts/sidebar/TabViews/SimpleList.ts
+++ ts/sidebar/TabViews/SimpleList.ts
@@ -4,36 +4,49 @@ type Bookmark = browser.bookmarks.BookmarkTreeNode;
 
 export default class SimpleList extends TabView {
 
-    private list:HTMLOListElement = null;
+	private list:HTMLOListElement = null;
 
-    constructor(sessionId:string) {
-        super(sessionId);
-        
-    }
+	constructor(sessionId:string) {
+		super(sessionId);
+		
+	}
 
-    public createHTML(tabBookmarks:Bookmark[]): HTMLOListElement {
-        let ol = document.createElement("ol");
-        this.list = ol;
+	public createHTML(tabBookmarks:Bookmark[]): HTMLOListElement {
+		let ol = document.createElement("ol");
+		this.list = ol;
+		this.setTabCountClass(tabBookmarks.length);
 
-        tabBookmarks.forEach(tab => {
-            let li:HTMLLIElement = document.createElement("li");
+		tabBookmarks.forEach(tab => {
+			let li:HTMLLIElement = document.createElement("li");
 
-            let a:HTMLAnchorElement = document.createElement("a");
-            a.classList.add("tab");
-            a.textContent = tab.title;
-            a.dataset.id = tab.id;
-            a.href = tab.url;
+			let a:HTMLAnchorElement = document.createElement("a");
+			a.classList.add("tab");
+			a.textContent = tab.title;
+			a.dataset.id = tab.id;
+			a.href = tab.url;
 
-            a.onclick = e => e.preventDefault();
+			a.onclick = e => e.preventDefault();
 
-            li.appendChild(a);
-            ol.appendChild(li);
-        });
+			li.appendChild(a);
+			ol.appendChild(li);
+		});
 
-        return this.list;
-    }
+		return this.list;
+	}
 
-    public update(tabBookmarks:Bookmark[]) {
+	private setTabCountClass(n:number):void {
+		if(n >= 10 && n < 100) {
+			this.list.classList.add("geq10");
+			this.list.classList.remove("geq100");
+		} else if(n >= 100) {
+			this.list.classList.add("geq100");
+		} else {
+			this.list.classList.remove("geq10");
+			this.list.classList.remove("geq100");
+		}
+	}
 
-    }
+	public update(tabBookmarks:Bookmark[]) {
+
+	}
 }
