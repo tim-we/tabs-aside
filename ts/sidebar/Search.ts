fd5d4fe80f3482848de6f8e291b5ee09575e0105
Tim Weißenfels <stu126998@mail.uni-kiel.de>
2018-07-09 19:39:41 +0200

    search bug fix #30

--- ts/sidebar/Search.ts
+++ ts/sidebar/Search.ts
@@ -1,44 +1,44 @@
 import { Message } from "../core/Messages";
 
 document.addEventListener("DOMContentLoaded", () => {
-    let searchInput:HTMLInputElement = document.getElementById("search-input") as HTMLInputElement;
-    let noResultsInfo:HTMLElement = document.getElementById("search-no-results");
-    noResultsInfo.textContent = browser.i18n.getMessage("sidebar_search_noresults");
+	let searchInput:HTMLInputElement = document.getElementById("search-input") as HTMLInputElement;
+	let noResultsInfo:HTMLElement = document.getElementById("search-no-results");
+	noResultsInfo.textContent = browser.i18n.getMessage("sidebar_search_noresults");
 
-    searchInput.placeholder = browser.i18n.getMessage("sidebar_search_placeholder");
-    searchInput.setAttribute("aria-label", browser.i18n.getMessage("sidebar_search_placeholder"));
-    searchInput.value = "";
-    searchInput.focus();
+	searchInput.placeholder = browser.i18n.getMessage("sidebar_search_placeholder");
+	searchInput.setAttribute("aria-label", browser.i18n.getMessage("sidebar_search_placeholder"));
+	searchInput.value = "";
+	searchInput.focus();
 
-    searchInput.addEventListener("input", async () => {
-        let query:string = searchInput.value.trim();
+	searchInput.addEventListener("input", async () => {
+		let query:string = searchInput.value.trim();
 
-        if(query === "") {
-            showAll();
-        } else {
-            let results:Set<string> = await search(query);
+		if(query === "") {
+			showAll();
+		} else {
+			let results:Set<string> = await search(query);
 
-            filterSessions(results);
+			filterSessions(results);
 
-            if(results.size === 0) {
-                noResultsInfo.classList.add("show");
-            } else {
-                noResultsInfo.classList.remove("show");
-            }
-        }
-    });
+			if(results.size === 0) {
+				noResultsInfo.classList.add("show");
+			} else {
+				noResultsInfo.classList.remove("show");
+			}
+		}
+	});
 
-    document.getElementById("search-icon").addEventListener("click", () => {
-        searchInput.focus();
-    });
+	document.getElementById("search-icon").addEventListener("click", () => {
+		searchInput.focus();
+	});
 
-    window.addEventListener("keydown", e => {
-        if(e.keyCode == 70 && e.ctrlKey) { // CTRL + F
-            e.preventDefault();
+	window.addEventListener("keydown", e => {
+		if(e.keyCode == 70 && e.ctrlKey) { // CTRL + F
+			e.preventDefault();
 
-            searchInput.focus();
-        }
-    });
+			searchInput.focus();
+		}
+	});
 });
 
 let rootId:string;
@@ -49,14 +49,14 @@ let sessionIds:Set<string> = new Set<string>();
 let sessionContainer:HTMLElement;
 
 export async function init(sessionRootId:string, container:HTMLElement) {
-    rootId = sessionRootId;
-    sessionContainer = container;
+	rootId = sessionRootId;
+	sessionContainer = container;
 
-    // initialize sessionIds
-    let sessionBookmarks = await browser.bookmarks.getChildren(rootId);  
-    sessionBookmarks.forEach(
-        session => sessionIds.add(session.id)
-    )
+	// initialize sessionIds
+	let sessionBookmarks = await browser.bookmarks.getChildren(rootId);
+	sessionBookmarks.forEach(
+		session => sessionIds.add(session.id)
+	);
 }
 
 /**
@@ -65,45 +65,47 @@ export async function init(sessionRootId:string, container:HTMLElement) {
  * @returns returns the results as a set of ids as a promise
  */
 async function search(query:string):Promise<Set<string>> {
-    let results:Set<string> = new Set<string>();
+	let results:Set<string> = new Set<string>();
 
-    let browserResults = await browser.bookmarks.search(query);
+	let browserResults = await browser.bookmarks.search(query);
 
-    browserResults.forEach(bm => {
-        // ignore root node
-        if(!bm.parentId) { return; }
+	browserResults.forEach(bm => {
+		// ignore root node
+		if(!bm.parentId) { return; }
 
-        // is this bookmark a session or part of a session ?
-        if(sessionIds.has(bm.id) || sessionIds.has(bm.parentId)) {
-            results.add(bm.id);
-        }
-    });
+		// is this bookmark a session or part of a session ?
+		if(sessionIds.has(bm.id)) {
+			results.add(bm.id);
+		} else if(sessionIds.has(bm.parentId)) {
+			results.add(bm.parentId);
+		}
+	});
 
-    return results;
+	return results;
 }
 
 function filterSessions(filter:Set<string>):void {
-    let sessionViews = sessionContainer.querySelectorAll<HTMLElement>(".session");
+	let sessionViews = sessionContainer.querySelectorAll<HTMLElement>(".session");
 
-    sessionViews.forEach(session => {
-        let id:string = session.dataset.id || "";
+	sessionViews.forEach(session => {
+		let id:string = session.dataset.id || "";
 
-        if(filter.has(id)) {
-            session.classList.remove("hidden");
-        } else {
-            session.classList.add("hidden");
-        }
-    });
+		if(filter.has(id)) {
+			session.classList.remove("hidden");
+		} else {
+			session.classList.add("hidden");
+		}
+	});
 }
 
 function showAll() {
-    let sessionViews = sessionContainer.querySelectorAll<HTMLElement>(".session");
+	let sessionViews = sessionContainer.querySelectorAll<HTMLElement>(".session");
 
-    sessionViews.forEach(session => {
-        session.classList.remove("hidden");
-    });
+	sessionViews.forEach(session => {
+		session.classList.remove("hidden");
+	});
 }
 
 browser.runtime.onMessage.addListener((message:Message) => {
-    // TODO: listen for session updates and update sessionIds
+	// TODO: listen for session updates and update sessionIds
 });
