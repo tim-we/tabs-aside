82406549deb83960bb911d5aa1772125ab5e54aa
Tim Weißenfels <tim.weissenfels@outlook.com>
2018-07-17 20:03:38 +0200

    new HTML utils

--- ts/util/HTMLUtilities.ts
+++ ts/util/HTMLUtilities.ts
@@ -1,5 +1,28 @@
 let whitespaceBetweenTags:RegExp = /\>\s+\</g;
 
 export function clean(html:string):string {
-    return html.replace(whitespaceBetweenTags, "><").trim();
+	return html.replace(whitespaceBetweenTags, "><").trim();
+}
+
+/**
+ * Localizes HTML that have the `.i18n` class
+ * @param container Container that will be searched for `.i18n` class members
+ */
+export function i18n(container:HTMLElement = document.body):void {
+	container.querySelectorAll("[data-i18n]")
+	.forEach((elem:HTMLElement) => {
+		let messageName:string = elem.dataset.i18n;
+		
+		elem.textContent = browser.i18n.getMessage(messageName);
+	});
+}
+
+export function DOMReady():Promise<void> {
+	if(document.readyState === "loading") {
+		return new Promise(resolve =>
+			document.addEventListener("DOMContentLoaded", () => {resolve()})
+		);
+	}
+
+	return Promise.resolve();
 }
