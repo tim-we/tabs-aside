e70556daa2ad2cbbf5c555b5c2ccb42f365cfc94
Tim Weißenfels <tim.weissenfels@outlook.com>
2018-07-19 17:59:02 +0200

    tab selector: selectable tabs

--- ts/tab-selector/TabSelector.ts
+++ ts/tab-selector/TabSelector.ts
@@ -1,7 +1,84 @@
-import * as HTMLUtils from "../util/HTMLUtilities";
+import * as View from "./View";
 
-HTMLUtils.DOMReady().then(() => {
-	console.log("localizing");
-	// apply localization
-	HTMLUtils.i18n();
-});
+export type Tab = browser.tabs.Tab;
+export type SelectableTab = Tab & {selected?:boolean};
+export type TabId = number;
+type Tabs = Map<TabId, SelectableTab>;
+
+let tabs:Tabs = new Map();
+
+async function init() {
+	await View.init();
+
+	// get all browser tabs
+	let browserTabs = await browser.tabs.query({});
+
+	browserTabs.forEach(tab => {
+		tabs.set(tab.id, tab);
+		View.add(tab);
+	});
+
+	browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+		let oldTab = tabs.get(tabId);
+
+		if(oldTab) {
+			let selectableTab:SelectableTab = tab;
+			selectableTab.selected = !!oldTab.selected;
+			tabs.set(tabId, selectableTab);
+
+			View.update(selectableTab);
+		}
+	});
+
+	browser.tabs.onCreated.addListener(tab => {
+		tabs.set(tab.id, tab);
+		View.add(tab);
+	});
+
+	browser.tabs.onRemoved.addListener(tabId => {
+		let tab = tabs.get(tabId);
+
+		if(tab) {
+			tabs.delete(tabId);
+			View.remove(tab);
+		}
+	});
+}
+
+export function toggleTab(tabId:TabId, updateView:boolean = false):boolean {
+	let tab:SelectableTab = tabs.get(tabId);
+	tab.selected = !tab.selected;
+
+	if(updateView) {
+		View.update(tab);
+	}
+
+	return tab.selected;
+}
+
+export function selectAll():void {
+	tabs.forEach(tab => {
+		if(!tab.selected) {
+			tab.selected = true;
+			View.update(tab);
+		}
+	});
+}
+
+export function unSelectAll():void {
+	tabs.forEach(tab => {
+		if(tab.selected) {
+			tab.selected = false;
+			View.update(tab);
+		}
+	});
+}
+
+export function invertSelection():void {
+	tabs.forEach(tab => {
+		tab.selected = !tab.selected;
+		View.update(tab);
+	});
+}
+
+init();
---
+++ ts/tab-selector/View.ts
@@ -0,0 +1,93 @@
+import * as HTMLUtils from "../util/HTMLUtilities";
+import { Tab, TabId, SelectableTab, toggleTab, selectAll, unSelectAll, invertSelection } from "./TabSelector";
+
+let tabs:Map<TabId, HTMLElement> = new Map();
+
+let tabsContainer:HTMLElement;
+
+export function init() {
+	return HTMLUtils.DOMReady().then(() => {
+		tabsContainer = document.getElementById("tabs");
+
+		// apply localization
+		HTMLUtils.i18n();
+
+		// selection control buttons
+		let selectControls:HTMLElement = document.getElementById("select-controls");
+		selectControls.querySelector("#select-all").addEventListener("click", selectAll);
+		selectControls.querySelector("#clear").addEventListener("click", unSelectAll);
+		selectControls.querySelector("#invert").addEventListener("click", invertSelection);
+	});
+}
+
+export function add(tab:SelectableTab) {
+	let view:HTMLElement = createTabView(tab);
+	tabsContainer.appendChild(view);
+	tabs.set(tab.id, view);
+}
+
+export function update(tab:SelectableTab) {
+	let view:HTMLElement = tabs.get(tab.id);
+	let newView:HTMLElement = createTabView(tab);
+
+	view.parentElement.replaceChild(newView, view);
+	tabs.set(tab.id, newView);
+}
+
+export function remove(tab:SelectableTab) {
+	let view:HTMLElement = tabs.get(tab.id);
+
+	if(view) {
+		tabs.delete(tab.id);
+		view.remove();
+	}
+}
+
+function createTabView(tab:Tab):HTMLElement {
+	let html:HTMLElement = document.createElement("div");
+	html.classList.add("tab");
+	html.title = tab.title;
+	html.dataset.id = tab.id+"";
+
+	if(tab.selected) {
+		html.classList.add("selected");
+	}
+
+	// favicon
+	if (tab.favIconUrl) {
+		let img = new Image();
+
+		// if image can not be loaded (e.g. broken or not found)
+		img.onerror = () => {
+			img.remove();
+			html.classList.add("no-favicon");
+		};
+
+		img.src = tab.favIconUrl;
+		img.classList.add("favicon");
+		img.alt = "icon";
+
+		html.appendChild(img);
+	} else {
+		html.classList.add("no-favicon");
+	}
+
+	// tab title
+	let title:HTMLElement = document.createElement("div");
+	title.classList.add("tab-title");
+	title.textContent = tab.title;
+	html.appendChild(title);
+
+	// click handler
+	html.addEventListener("click", () => {
+		let selected:boolean = toggleTab(tab.id);
+
+		if(selected) {
+			html.classList.add("selected");
+		} else {
+			html.classList.remove("selected");
+		}
+	});
+
+	return html;
+}
