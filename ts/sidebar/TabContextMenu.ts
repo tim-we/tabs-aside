e5b8ca4750d7e3ee7c35b41c6b2a78e3a9eb350d
Tim Weißenfels <tim.weissenfels@outlook.com>
2018-10-17 01:46:27 +0200

    #28 basic implementation

--- ts/sidebar/TabContextMenu.ts
+++ ts/sidebar/TabContextMenu.ts
@@ -4,6 +4,7 @@ import * as Clipboard from "../util/Clipboard";
 import SessionView from "./SessionView";
 import { SessionCommand } from "../messages/Messages";
 import TabView from "./TabViews/TabView";
+import TabData from "../core/TabData";
 
 let _i18n = browser.i18n.getMessage;
 
@@ -16,7 +17,16 @@ export default class TabContextMenu extends OverlayMenu {
 		}, "options-menu-tab-copy");
 
 		if(!session.isActive()) {
-			this.addItem("sidebar_tab_remove_from_session", async () => {
+			this.addItem("sidebar_tab_open_remove", async () => {
+				let createProps = TabData.createFromBookmark(tabBookmark).getTabCreateProperties(true);
+
+				//TODO: find a window that is not associated with a session
+				await browser.tabs.create(createProps);
+
+				SessionCommand.send("remove-tab", [tabBookmark.id]);
+			});
+
+			this.addItem("sidebar_tab_remove_from_session", () => {
 				SessionCommand.send("remove-tab", [tabBookmark.id]);
 			}, "options-menu-tab-remove");
 		}
