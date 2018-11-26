e1fe663175d9842913602129b0094d326f0f0d49
jens <kontakt@jenstangermann.com>
2018-11-26 18:53:21 +0100

    bundler removed, gulp-typescript added

--- gulp/config.js
+++ gulp/config.js
@@ -21,75 +21,8 @@ module.exports = {
   },
 
   js: {
-    dest: 'js',
-    bundles: [
-      {
-        name: 'background.js',
-        src: [{
-          type: 'browserify',
-          entryFile: 'ts/background/background.ts'
-        }]
-      },
-      {
-        name: 'bmSelector.js',
-        src: [{
-          type: 'browserify',
-          entryFile: 'ts/bookmark-selector/Controller.ts'
-        }]
-      },
-      {
-        name: 'menu.js',
-        src: [{
-          type: 'browserify',
-          entryFile: 'ts/browserAction/Menu.ts'
-        }]
-      },
-      {
-        name: 'options.js',
-        src: [{
-          type: 'browserify',
-          entryFile: 'ts/options/OptionsPage.ts'
-        }]
-      },
-      {
-        name: 'sidebar.js',
-        src: [{
-          type: 'browserify',
-          entryFile: 'ts/sidebar/sidebar.ts'
-        }]
-      },
-      {
-        name: 'tabSelector.js',
-        src: [{
-          type: 'browserify',
-          entryFile: 'ts/browserAction/TabSelector.ts'
-        }]
-      },
-      {
-        name: 'privileged.js',
-        src: [{
-          type: 'browserify',
-          entryFile: 'ts/extension-pages/privileged.ts'
-        }]
-      }
-    ],
-    browserify: {
-      paths: ['./node_modules', './src/ts'],
-      extensions: ['.ts']
-    },
-    babelify: {
-      presets: [
-        ['@babel/preset-env', {
-          'useBuiltIns': 'usage'
-        }],
-        ['@babel/preset-typescript']
-      ],
-      plugins: [
-        '@babel/proposal-class-properties',
-        '@babel/proposal-object-rest-spread'
-      ],
-      extensions: ['.ts','.js']
-    }
+    src: 'ts/**/*.ts',
+    dest: 'js'
   }
 
 };
--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,94 +1,16 @@
 const del = require('del');
 const gulp = require('gulp');
-const gulpif = require('gulp-if');
-const concat = require('gulp-concat');
-const browserify = require('browserify');
-const watchify = require('watchify');
-const babelify = require('babelify');
-const sourcestream = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
-const mergeStreams = require('merge-stream');
-const sourcemaps = require('gulp-sourcemaps');
-const tap = require('gulp-tap');
+const ts = require('gulp-typescript');
 
 const path = require('path');
-const fs = require('fs');
 
 const log = require('../lib/log');
 const config = require('../config.js');
 
 const task = {};
+const srcPath = path.resolve(config.srcPath, config.js.src);
 const destPath = path.resolve(config.destPath, config.js.dest);
-
-// browserify
-const getBundler = (type, source) => {
-  let args = {
-    debug: config.env === 'dev',
-    paths: config.js.browserify.paths,
-    extensions: config.js.browserify.extensions
-  };
-  if (type === 'watch') {
-    args = Object.assign(args, watchify.args);
-  }
-  const b = browserify(path.resolve(config.srcPath, source.entryFile), args)
-    .transform(babelify, {
-      presets: config.js.babelify.presets,
-      plugins: config.js.babelify.plugins,
-      extensions: config.js.babelify.extensions
-    });
-  return type === 'watch' ? watchify(b) : b;
-};
-
-// generate bundle
-const createBundle = (bundlerType, bundleConfig) => {
-  const sourceStreams = [];
-
-  bundleConfig.src.forEach(source => {
-    switch (source.type) {
-      case 'concat':
-        source.files.forEach(fileSrc => {
-          sourceStreams.push(gulp.src(
-            path.resolve(config.srcPath, fileSrc)
-          ));
-        });
-      break;
-
-      case 'browserify':
-        let bundler;
-        if (bundlerType === 'watch') {
-          bundler =  getBundler('watch', source);
-          bundler.on('update', file => {
-            createBundle('watch', bundleConfig);
-          });
-          bundler.on('log', () => {
-            log('info', 'js:build', `Build "${bundleConfig.name}".`);
-          });
-        }
-        else {
-          bundler = getBundler('default', source);
-        }
-        sourceStreams.push(
-          bundler
-            .bundle()
-            .on('error', error => {
-              log('error', 'js:build', `Browserify: [${error.name}] ${error.message}`);
-            })
-            .pipe(sourcestream(bundleConfig.name))
-            .pipe(buffer())
-        );
-      break;
-    }
-  });
-
-  return mergeStreams(...sourceStreams)
-    .pipe(gulpif(config.env === 'dev', sourcemaps.init({ loadMaps: true })))
-    .pipe(concat(bundleConfig.name))
-    .pipe(tap(file => {
-      if (bundlerType !== 'watch') log('info', 'js:build', `Build "${file.basename}".`);
-    }))
-    .pipe(gulpif(config.env === 'dev', sourcemaps.write('.')))
-    .pipe(gulp.dest(destPath));
-};
+const tsProject = ts.createProject('tsconfig.json');
 
 task['js:clean'] = () => {
   return del(destPath).then(paths => {
@@ -99,25 +21,13 @@ task['js:clean'] = () => {
 };
 
 task['js:build'] = () => {
-  const streams = config.js.bundles.map(bundleConfig => {
-    return createBundle('default', bundleConfig);
-  });
-  return mergeStreams(...streams);
+  const tsResult = gulp.src(srcPath)
+    .pipe(tsProject());
+  return tsResult.js.pipe(gulp.dest(destPath));
 };
 
 task['js:watch:init'] = (done) => {
-  config.js.bundles.forEach(bundleConfig => {
-    createBundle('watch', bundleConfig);
-
-    bundleConfig.src.forEach(source => {
-      if (source.type === 'concat') {
-        gulp.watch(source.files, function js() { return createBundle('default', bundleConfig) })
-        .on('change', path => {
-          log('info', 'js:watch', `File "${path}" has changed.`);
-        });
-      }
-    });
-  });
+  gulp.watch(srcPath, gulp.series('js:build'));
   done();
 };
 
