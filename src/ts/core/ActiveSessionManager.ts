e250c7132c2a4db5aa32f37715bf0e5a1ecef25e
Tim Weißenfels <tim.weissenfels@outlook.com>
2018-12-09 02:33:10 +0100

    fixed position of new sessions in sidebar

--- src/ts/core/ActiveSessionManager.ts
+++ src/ts/core/ActiveSessionManager.ts
@@ -1,5 +1,5 @@
 import ActiveSession, { ActiveSessionData } from "./ActiveSession.js";
-import { Tab, Bookmark, SessionId } from "../util/Types.js";
+import { Tab, Bookmark, SessionId, BookmarkCreateDetails } from "../util/Types.js";
 import { SessionCommand, SessionEvent, DataRequest, SessionContentUpdate } from "../messages/Messages.js";
 import * as OptionsManager from "../options/OptionsManager.js";
 import TabData from "./TabData.js";
@@ -17,6 +17,7 @@ export async function createSessionFromTabs(
 
 	// create bookmark folder
 	let folder:Bookmark = await browser.bookmarks.create({
+		index: 0,
 		parentId: await OptionsManager.getValue<string>("rootFolder"),
 		title: title
 	});
