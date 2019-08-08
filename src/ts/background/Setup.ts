3895b6d59b6e0c89c4d83e546a8bd10cfafa3ca6
Tim Weißenfels <tim.weissenfels@outlook.com>
2019-08-08 12:20:47 +0200

    Data migration from previous versions

---
+++ src/ts/background/Setup.ts
@@ -0,0 +1,61 @@
+import * as OptionsManager from "../options/OptionsManager.js";
+
+type StoredData = {
+	"version"?:number,
+	"bookmarkFolderID"?:string
+	"options"?:any,
+	"ba-icon"?:string
+};
+
+export async function isSetup():Promise<boolean> {
+	let data:StoredData = await browser.storage.local.get();
+
+	return data.version == 2;
+}
+
+export async function setup():Promise<void> {
+	let data:StoredData = await browser.storage.local.get();
+
+	let icon:string = "dark";
+	let root:string|null = null;
+
+	if(data.version == 1) {
+		if(data["bookmarkFolderID"]) {
+			await browser.bookmarks.get(data["bookmarkFolderID"]).then(
+				bms => {
+					if(bms[0].type === "folder") {
+						root = bms[0].id;
+					}
+				}
+			).catch();
+		}
+
+		if(data["ba-icon"]) {
+			// use old browser action icon setting
+			if(data["ba-icon"] === "dynamic") {
+				icon = "context";
+			} else if(data["ba-icon"] === "light" || data["ba-icon"] === "dark") {
+				icon = data["ba-icon"];
+			}
+
+			browser.storage.local.remove("ba-icon");
+		}
+	} else if(data.version != 2) {
+		// removing the extension will also remove all the stored data
+		// lets check if there exists a "Tabs Aside" folder from a previous installation
+		let data = (await browser.bookmarks.search({title:"Tabs Aside"}))
+					.filter(bm => bm.type === "folder");
+
+		if(data.length > 0) {
+			// folder found
+			root = data[0].id;
+			console.log("[TA] Found 'Tabs Aside' folder from a previous installation.");
+		}
+	}
+
+	if(root !== null) {
+		await OptionsManager.setValue("rootFolder", root);
+	}
+
+	await OptionsManager.setValue("browserActionIcon", icon);
+}
--- src/ts/background/background.ts
+++ src/ts/background/background.ts
@@ -4,28 +4,37 @@ import * as BrowserActionManager from "../browserAction/BrowserActionManager.js"
 import * as KeyboardCommands from "./KeyboardCommands.js";
 import * as MessageListener from "../messages/MessageListener.js";
 import * as BrowserTabContextMenu from "./BrowserTabContextMenu.js";
+import { isSetup, setup } from "./Setup.js";
 
 MessageListener.setDestination("background");
-BrowserActionManager.init();
-KeyboardCommands.init();
 
-SessionManager.init().then(() => {
-	MessageListener.add("*", async (message:Message) => {
-		if(message.type === "SessionCommand") {
-			let cmd:SessionCommand = message as SessionCommand;
-	
-			return await SessionManager.execCommand(cmd);
-		}
-	});
-	
-	browser.runtime.onMessage.addListener((message:Message) => {
-		if(message.type === "DataRequest") {
-			let req:DataRequest = message as DataRequest;
-	
-			return SessionManager.dataRequest(req);
-		}
-	});
-});
+(async function(){
+	if(await isSetup()) {
+		BrowserActionManager.init();
+		KeyboardCommands.init();
+
+		SessionManager.init().then(() => {
+			MessageListener.add("*", async (message:Message) => {
+				if(message.type === "SessionCommand") {
+					let cmd:SessionCommand = message as SessionCommand;
+			
+					return await SessionManager.execCommand(cmd);
+				}
+			});
+			
+			browser.runtime.onMessage.addListener((message:Message) => {
+				if(message.type === "DataRequest") {
+					let req:DataRequest = message as DataRequest;
+			
+					return SessionManager.dataRequest(req);
+				}
+			});
+		});
 
 
-BrowserTabContextMenu.init();
+		BrowserTabContextMenu.init();
+	} else {
+		setup();
+	}
+})();
+
