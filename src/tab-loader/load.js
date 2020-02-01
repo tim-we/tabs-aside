a6b79394440bfa10c2bf724d4fc8ce03bd2322f3
Tim Weißenfels <tim.weissenfels@outlook.com>
2020-02-01 10:42:44 +0100

    Move all legacy tab loader code to its own folder

--- src/tab-loader/load.js
+++ src/tab-loader/load.js
@@ -1,16 +1,13 @@
 (function () {
-	let params = new URLSearchParams(document.location.search.substring(1));
+    let params = new URLSearchParams(document.location.search.substring(1));
     let url = new URL(params.get("url"));
-    
+
     console.log("[TA] Found an unloaded tab from a previous version.", url);
 
-    let a = document.getElementById("load-manually") as HTMLAnchorElement;
+    let a = document.getElementById("load-manually");
     a.href = url.href;
     document.getElementById("url").textContent = url.href;
 
     // try to load this URL
-    //window.location.href = url.href;
+    window.location.href = url.href;
 })();
-
-// dummy export
-export var x = 42;
