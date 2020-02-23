66cff1be089f9fb546cacd317754d05d76cdef8c
Tim Weißenfels <tim.weissenfels@outlook.com>
2020-02-23 23:28:22 +0100

    Remove command option type

--- src/ts/options/OptionTypeDefinition.d.ts
+++ src/ts/options/OptionTypeDefinition.d.ts
@@ -1,31 +1,27 @@
 export interface GenericOption<S extends string, T> {
-	id: string;
-	type: S;
-	default: T;
+    id: string;
+    type: S;
+    default: T;
 }
 
 export interface SelectOption extends GenericOption<"select", string> {
-	options: string[];
+    options: string[];
 }
 
-type OptionsGroup = "appearance" | "core" | "keyboard";
+type OptionsGroup = "appearance" | "core";
 
 interface DisplayOptions {
-	hint?:boolean; // tooltip
-	info?:boolean; // html
-	hidden?:boolean;
-	group?:OptionsGroup;
-	activeOnly?:boolean; // requires active sessions to be on/off (true/false)
+    hint?:boolean; // tooltip
+    info?:boolean; // html
+    hidden?:boolean;
+    group?:OptionsGroup;
+    activeOnly?:boolean; // requires active sessions to be on/off (true/false)
 }
 
 export type BooleanOption = GenericOption<"boolean", boolean>;
-
 export type BookmarkOption = GenericOption<"bookmark", string>;
 
-export type CommandOption = GenericOption<"command", string>;
-
 export type Option = (SelectOption
-					| CommandOption
-					| BooleanOption
-					| BookmarkOption)
-					& DisplayOptions;
+                    | BooleanOption
+                    | BookmarkOption)
+                    & DisplayOptions;
