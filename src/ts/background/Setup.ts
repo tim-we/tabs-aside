668e033c10af0dddeab20803bcc39bb24aa6390f
Tim Weißenfels <10491533+tim-we@users.noreply.github.com>
2020-04-21 23:14:06 +0200

    Rename option: ignorePinned -> asidePinnedTabs #74

--- src/ts/background/Setup.ts
+++ src/ts/background/Setup.ts
@@ -3,71 +3,87 @@ import * as BrowserActionManager from "../browserAction/BrowserActionManager.js"
 import { attempt } from "../util/PromiseUtils.js";
 
 type StoredData = {
-	"version"?:number,
-	"options"?:any, // user options
-	"setup"?:boolean, // (user) setup completed flag
-	"bookmarkFolderID"?:string // legacy
-	"ba-icon"?:string // legacy
+    "version"?:number,
+    "options"?:any, // user options
+    "setup"?:boolean, // (user) setup completed flag
+    "bookmarkFolderID"?:string // legacy
+    "ba-icon"?:string // legacy
 };
 
-export async function isSetup():Promise<boolean> {
-	let data:StoredData = await browser.storage.local.get() || {};
+const CURRENT_VERSION = 3;
 
-	return data.version === 2 && data.setup === true;
+export async function isSetup():Promise<boolean> {
+    let data:StoredData = await browser.storage.local.get() || {};
+
+    return data.version === CURRENT_VERSION && data.setup === true;
 }
 
 export async function setup():Promise<void> {
-	let data:StoredData = await browser.storage.local.get() || {};
+    let data:StoredData = await browser.storage.local.get() || {};
+    console.assert(data.version !== CURRENT_VERSION);
 
-	let root:string|null = null;
+    let action:"setup"|"restart" = "setup";
+    let root:string|null = null;
 
-	// data migration
+    // data migration
 
-	if(data.version == 1) {
-		if(data["bookmarkFolderID"]) {
-			// this will not be removed to keep old versions working
-			await attempt(browser.bookmarks.get(data["bookmarkFolderID"]).then(
-				bms => {
-					if(bms[0].type === "folder") {
-						root = bms[0].id;
-					}
-				}
-			));
-		}
+    if(data.version == 2) {
+        action = "restart";
+        console.log("[TA] data.options", data.options);
+        if(data.options.ignorePinned !== undefined) {
+            await OptionsManager.setValue("asidePinnedTabs", !data.options.ignorePinned);
+        }
 
-		if(data["ba-icon"]) {
-			// use old browser action icon setting
-			if(data["ba-icon"] === "dynamic") {
-				OptionsManager.setValue<boolean>("browserActionContextIcon", true);
-			}
+        return;
+    } else if(data.version == 1) {
+        if(data["bookmarkFolderID"]) {
+            // this will not be removed to keep old versions working
+            await attempt(browser.bookmarks.get(data["bookmarkFolderID"]).then(
+                bms => {
+                    if(bms[0].type === "folder") {
+                        root = bms[0].id;
+                    }
+                }
+            ));
+        }
 
-			browser.storage.local.remove("ba-icon");
-		}
-	} else if(data.version != 2) {
-		// removing the extension will also remove all the stored data
-		// lets check if there exists a "Tabs Aside" folder from a previous installation
-		let folders = (await browser.bookmarks.search({title:"Tabs Aside"}))
-					.filter(bm => bm.type === "folder");
+        if(data["ba-icon"]) {
+            // use old browser action icon setting
+            if(data["ba-icon"] === "dynamic") {
+                OptionsManager.setValue<boolean>("browserActionContextIcon", true);
+            }
 
-		if(folders.length > 0) {
-			// folder found
-			root = folders[0].id;
-		}
-	}
+            browser.storage.local.remove("ba-icon");
+        }
+    } else {
+        // removing the extension will also remove all the stored data
+        // lets check if there exists a "Tabs Aside" folder from a previous installation
+        let folders = (await browser.bookmarks.search({title:"Tabs Aside"}))
+                    .filter(bm => bm.type === "folder");
 
-	if(root !== null) {
-		console.log("[TA] Found 'Tabs Aside' folder from a previous installation.");
-		await OptionsManager.setValue("rootFolder", root);
-	}
+        if(folders.length > 0) {
+            // folder found
+            root = folders[0].id;
+        }
+    }
 
-	await browser.storage.local.set({"version": 2});
+    if(root !== null) {
+        console.log("[TA] Found 'Tabs Aside' folder from a previous installation.");
+        await OptionsManager.setValue("rootFolder", root);
+    }
 
-	// update browser action icon
-	BrowserActionManager.init();
+    await browser.storage.local.set({"version": CURRENT_VERSION});
 
-	// prepare user setup
-	BrowserActionManager.showSetup();
-	browser.sidebarAction.setPanel({
-		panel: browser.runtime.getURL("html/user-setup.html")
-	});
+    // update browser action icon
+    BrowserActionManager.init();
+
+    if(action === "setup") {
+        // prepare user setup
+        BrowserActionManager.showSetup();
+        browser.sidebarAction.setPanel({
+            panel: browser.runtime.getURL("html/user-setup.html")
+        });
+    } else if(action === "restart") {
+        //TODO
+    }
 }
